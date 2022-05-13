96694c4d9118f071e34b80d07a26446e7287aadc
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-05-13 17:24:55 +0500

    initilize spfx react project

---
+++ src/webparts/documentCardExampleWebPart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IDocumentCardExampleWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'DocumentCardExampleWebPartWebPartStrings' {
+  const strings: IDocumentCardExampleWebPartWebPartStrings;
+  export = strings;
+}
